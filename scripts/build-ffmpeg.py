7b51a4bf07839587c0fb9a482b2f0378844d68f2
samicrusader <hi@samicrusader.me>
2023-10-05 05:16:32 -0400

    Update build-ffmpeg.py

--- scripts/build-ffmpeg.py
+++ scripts/build-ffmpeg.py
@@ -320,7 +320,7 @@ if not os.path.exists(output_tarball):
                 "xvid",
                 "xz",
             ],
-            source_url="https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.xz",
+            source_url="https://ffmpeg.org/releases/ffmpeg-6.0.tar.xz",
             build_arguments=[
                 "--disable-alsa",
                 "--disable-doc",
